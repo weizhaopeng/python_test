f4981188db52a6271bcd59053c6259e3801c5209
weizhaopeng <wzp_1995@163.com>
2019-11-24 10:51:03 +0800

    完成筛选出字符串的后缀

--- test/virate_test.py
+++ test/virate_test.py
@@ -1,4 +1,7 @@
+import random
 import time
+
+"""跑马灯程序"""
 
 
 class Marquee(object):
@@ -13,6 +16,7 @@ class Marquee(object):
         self.marqueeUpdateTime = Marquee.INIT_UPDATE_TIME
         self.marqueeCycleTimes = Marquee.INIT_CYCLE_TIMES
 
+    # 跑马灯展示
     def marqueeDisplay(self):
         strLength = len(self.marqueeContent)
         strList = list(self.marqueeContent)
@@ -21,7 +25,7 @@ class Marquee(object):
         cycleTimes = self.marqueeCycleTimes * self.marqueeCapability
         while 0 <= cycleTimes:
             # 伪清屏
-            marquee._cls()
+            Marquee._cls()
             self._marqueePrint(strList)
             time.sleep(self.marqueeUpdateTime)
             self._marqueeUpdateStrList(strList)
@@ -41,6 +45,45 @@ class Marquee(object):
         print(80 * "\n")
 
 
+"""生成指定长度的验证码，只能是数字和字母"""
+
+
+class VerificationCode(object):
+    INIT_CODE_LENGTH = 6
+    CODE_RANGE = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+    @staticmethod
+    def generateCode(codeLength=INIT_CODE_LENGTH):
+        VcodeGeneraged = ""
+        for posi in range(codeLength):
+            codeIndex = random.randint(0, len(VerificationCode.CODE_RANGE) - 1)
+            VcodeGeneraged += VerificationCode.CODE_RANGE[codeIndex]
+        return VcodeGeneraged
+
+
+"""给定一个文件名，然后返回文件名的后缀"""
+
+
+class File(object):
+    @staticmethod
+    def resolveFileSuffix(filename=""):
+        if "" == filename:
+            return -2
+        fileNameReversed = filename[::-1]
+        index = fileNameReversed.find(".")
+
+        if -1 == index:
+            return -1
+        else:
+            return filename[len(filename) - index:]
+
+
+
 if __name__ == "__main__":
-    marquee = Marquee("Welcome to the world of Python")
-    marquee.marqueeDisplay()
+    # marquee = Marquee("Welcome to the world of Python")
+    # marquee.marqueeDisplay()
+
+    # vcode = VerificationCode.generateCode()
+    # print(vcode)
+
+    print(File.resolveFileSuffix("ni.haoexl"))
